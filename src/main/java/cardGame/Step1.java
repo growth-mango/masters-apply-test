624068b81e961c29498a3d91f845b6d77493cb08
mango <esudatcom@gmail.com>
2023-12-03 23:01:51 +0900

    docs: 1단계 구현 기능 목록 수정

--- src/main/java/cardGame/Step1.java
+++ src/main/java/cardGame/Step1.java
@@ -1,5 +1,22 @@
+import java.util.List;
+import java.util.Random;
+
 public class Step1 {
     public static void main(String[] args) {
         // 작성예정
+
+    }
+
+    // 3행 6열
+    public static int[][] generateNumbers() {
+        int[][] grid = new int[3][6];
+        int num = 1;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 6; j++) {
+                grid[i][j] = num;
+                num++;
+            }
+        }
     }
 }
